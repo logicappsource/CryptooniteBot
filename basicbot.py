56f624a18a8ce470a9e51a956f13364327df72b6
logicappsource <logicappsource@gmail.com>
2018-08-21 20:01:01 +0200

    Calculate Slippage price - Execute order if profitable

--- basicbot.py
+++ basicbot.py
@@ -1,55 +1,83 @@
-from catalyst.api import symbol, order
+from catalyst.api import symbol, order  #get_open_orders
 from catalyst.utils.run_algo import run_algorithm
 import pandas as pd
 
 
-def initialze(context):
+def is_profitable_after_fees(sell_price, buy_price, sell_market, buy_market):
+    sell_fee = get_fee(sell_market, sell_price)
+    buy_fee = get_fee(buy_market, buy_price)
+    expected_profit = sell_price - buy_price - sell_fee - buy_fee
+
+    if expected_profit > 0:
+        print("Sell {} at {}, Buy {} at {}".format(sell_market.name, sell_price, buy_market.name, buy_price))
+        print("Total fees: {}".format(buy_fee + sell_fee))
+        print("Expected profit: {}".format(expected_profit))
+        return True
+    return False
+
+
+def get_fee(market, price):
+    return round(market.api.fees['trading']['taker'] * price, 5)
+
+
+def get_adjusted_prices(price, slippage):
+    adj_sell_price = price * (1 - slippage)
+    adj_buy_price = price * (1 + slippage)
+    return adj_sell_price, adj_buy_price
+
+
+def initialize(context):
     context.bitfinex = context.exchanges['bitfinex']
     context.poloniex = context.exchanges['poloniex']
 
     context.bitfinex_trading_pair = symbol('eth_btc', context.bitfinex.name)
-    context.poloniex_trading_pair = symbol('eth_btc', context.poloniex.name)
+    context.poloniex_trading_pair = symbol('eth_btc', context.poloniex.name)  #  btc_usd , btc_usdt
 
 
 def handle_data(context, data):
     poloneix_price = data.current(context.poloniex_trading_pair, 'price')
     bitfinex_price = data.current(context.bitfinex_trading_pair, 'price')
+    slippage = 0.03
+    sell_p, buy_p = get_adjusted_prices(poloneix_price, slippage)
+    sell_b, buy_p = get_adjusted_prices(bitfinex_price, slippage)
 
-    print('Date: {}'.format(data.current_dt))
-    print('Poloniex: {}'.format(poloneix_price))
-    print('bitfinex: {}'.format(bitfinex_price))
+    if is_profitable_after_fees(sell_p, buy_p, context.poloniex, context.bitfinex):
 
-    if(poloneix_price > bitfinex_price):
-        print("Buy on Bittex  -> sell on bitfinex ")
+        print('Date: {}'.format(data.current_dt))
+        print('Bitfinex Price: {}, Poloniex Price: {}'.format(bitfinex_price, poloneix_price))
+
         order(asset=context.bitfinex_trading_pair,
               amount=1,
-              limit_price=bitfinex_price)
+              limit_price=buy_p)
 
         order(asset=context.poloniex_trading_pair,
               amount=-1,
-              limit_price=poloneix_price)
+              limit_price=sell_p)
 
-    elif (bitfinex_price > poloneix_price):
-        order(asset=context.bitfinex_trading_pair,
-              amount=-1,
-              limit_price=bitfinex_price)
+    elif is_profitable_after_fees(sell_b, buy_p, context.bitfinex, context.poloniex):
+        print('Date: {}'.format(data.current_dt))
+        print('Bitfinex Price: {}, Poloniex Price: {}'.format(bitfinex_price, poloneix_price))
         order(asset=context.poloniex_trading_pair,
+              amount=-1,
+              limit_price=buy_p)
+        order(asset=context.bitfinex_trading_pair,
               amount=1,
-              limit_price=poloneix_price)
+              limit_price=sell_b)
 
 
 def analyze(context, perf):
-    #
     pass
 
 
-perf = run_algorithm(capital_base=100,
-              initialize=initialze,
+#if __name__ == '__main__':
+perf = run_algorithm(capital_base=10,
+              initialize=initialize,
               handle_data=handle_data,
               analyze=analyze,
               live=False,
-              #base_currency='btc',
+             # base_currency='btc',
               exchange_name='bitfinex, poloniex',
+              algo_namespace='Cryptoonite',
               data_frequency='minute',
               start=pd.to_datetime('2017-12-12', utc=True),
               end=pd.to_datetime('2017-12-12', utc=True))
